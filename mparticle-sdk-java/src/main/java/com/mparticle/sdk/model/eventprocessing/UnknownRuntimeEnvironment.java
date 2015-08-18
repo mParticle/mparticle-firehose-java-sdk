86a22b78c1fc5322446f78906c5025f7db2309c2
Oleg Konoplyan <olviko@users.noreply.github.com>
2015-08-18 17:27:32 -0400

    support for unknown env

---
+++ mparticle-sdk-java/src/main/java/com/mparticle/sdk/model/eventprocessing/UnknownRuntimeEnvironment.java
@@ -0,0 +1,7 @@
+package com.mparticle.sdk.model.eventprocessing;
+
+public final class UnknownRuntimeEnvironment extends RuntimeEnvironment {
+    public UnknownRuntimeEnvironment() {
+        super(Type.UNKNOWN);
+    }
+}
