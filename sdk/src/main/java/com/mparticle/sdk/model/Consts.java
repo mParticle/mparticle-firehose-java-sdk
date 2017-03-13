9ebef40015ebcf21770176ebe31e136dd02b0765
Sam Dozor <sdozor@mparticle.com>
2017-03-13 16:20:37 -0400

    Update version to 1.2.3

--- sdk/src/main/java/com/mparticle/sdk/model/Consts.java
+++ sdk/src/main/java/com/mparticle/sdk/model/Consts.java
@@ -2,7 +2,7 @@ package com.mparticle.sdk.model;
 
 public final class Consts {
 
-    public static final String SDK_VERSION="1.2.2";
+    public static final String SDK_VERSION="1.2.3";
 
     private Consts() {
     }
