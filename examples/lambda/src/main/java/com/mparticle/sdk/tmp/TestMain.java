bae732742740184136ced9d99c91bf17fac26013
Sam Dozor <sdozor@mparticle.com>
2017-10-13 12:43:59 -0400

    Add JSON schema

--- examples/lambda/src/main/java/com/mparticle/sdk/tmp/TestMain.java
+++ examples/lambda/src/main/java/com/mparticle/sdk/tmp/TestMain.java
@@ -1,13 +1,24 @@
 package com.mparticle.sdk.tmp;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.module.jsonSchema.JsonSchemaGenerator;
+import com.fasterxml.jackson.module.jsonSchema.JsonSchema;
 import com.mparticle.sdk.model.Message;
 import com.mparticle.sdk.model.MessageSerializer;
+import com.mparticle.sdk.model.audienceprocessing.*;
 import com.mparticle.sdk.model.eventprocessing.*;
-import com.mparticle.sdk.model.registration.Account;
-import com.mparticle.sdk.model.registration.ModuleRegistrationRequest;
+import com.mparticle.sdk.model.registration.*;
 import com.mparticle.sdk.samples.SampleExtension;
+import com.sun.org.apache.xpath.internal.operations.And;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -16,6 +27,7 @@ import java.util.Map;
 
 public class TestMain {
     public static void main(String[] args) {
+        generateJsonSchema();
         //testSerializer();
         testRegister();
         //testEventProcessing();
@@ -36,6 +48,89 @@ public class TestMain {
             e.printStackTrace();
         }
 
+    }
+
+    private static void generateJsonSchema() {
+        ObjectMapper mapper = new ObjectMapper();
+        // ignore missing fields
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        // ignore nulls and empty containers
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+        // only include annotated fields
+        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
+        // use toString() for enums
+        mapper.configure(SerializationFeature.WRITE_ENUMS_USING_TO_STRING, true);
+        mapper.configure(DeserializationFeature.READ_ENUMS_USING_TO_STRING, true);
+        //registration
+        printSchema(Account.class, mapper);
+        printSchema(AudienceProcessingRegistration.class, mapper);
+        printSchema(BooleanSetting.class, mapper);
+        printSchema(DeviceIdentityPermission.class, mapper);
+        printSchema(EventProcessingRegistration.class, mapper);
+        printSchema(FloatSetting.class, mapper);
+        printSchema(IntegerSetting.class, mapper);
+        printSchema(ModuleRegistrationRequest.class, mapper);
+        printSchema(ModuleRegistrationResponse.class, mapper);
+        printSchema(Permissions.class, mapper);
+        printSchema(Setting.class, mapper);
+        printSchema(TextSetting.class, mapper);
+        printSchema(UserIdentityPermission.class, mapper);
+        //audience
+        printSchema(Audience.class, mapper);
+        printSchema(AudienceMembershipChangeRequest.class, mapper);
+        printSchema(AudienceMembershipChangeResponse.class, mapper);
+        printSchema(AudienceSubscriptionRequest.class, mapper);
+        printSchema(AudienceSubscriptionResponse.class, mapper);
+        printSchema(UserProfile.class, mapper);
+        //event processing
+        printSchema(AndroidRuntimeEnvironment.class, mapper);
+        printSchema(ApplicationStateTransitionEvent.class, mapper);
+        printSchema(AttributionEvent.class, mapper);
+        printSchema(CustomEvent.class, mapper);
+        printSchema(DeviceIdentity.class, mapper);
+        printSchema(ErrorEvent.class, mapper);
+        printSchema(Event.class, mapper);
+        printSchema(EventProcessingRequest.class, mapper);
+        printSchema(EventProcessingResponse.class, mapper);
+        printSchema(Identity.class, mapper);
+        printSchema(Impression.class, mapper);
+        printSchema(ImpressionEvent.class, mapper);
+        printSchema(IosRuntimeEnvironment.class, mapper);
+        printSchema(Location.class, mapper);
+        printSchema(PrivacySettingChangeEvent.class, mapper);
+        printSchema(Product.class, mapper);
+        printSchema(ProductActionEvent.class, mapper);
+        printSchema(Promotion.class, mapper);
+        printSchema(PromotionActionEvent.class, mapper);
+        printSchema(PushMessageReceiptEvent.class, mapper);
+        printSchema(PushSubscriptionEvent.class, mapper);
+        printSchema(RuntimeEnvironment.class, mapper);
+        printSchema(ScreenViewEvent.class, mapper);
+        printSchema(SessionEndEvent.class, mapper);
+        printSchema(SessionStartEvent.class, mapper);
+        printSchema(TVOSRuntimeEnvironment.class, mapper);
+        printSchema(UnknownRuntimeEnvironment.class, mapper);
+        printSchema(UserAttributeChangeEvent.class, mapper);
+        printSchema(UserIdentity.class, mapper);
+        printSchema(UserIdentityChangeEvent.class, mapper);
+        printSchema(ScreenViewEvent.class, mapper);
+        printSchema(WebRuntimeEnvironment.class, mapper);
+
+    }
+
+    private static void printSchema(Class<?> type, ObjectMapper mapper) {
+        JsonSchemaGenerator schemaGen = new JsonSchemaGenerator(mapper);
+        try {
+            JsonSchema schema = schemaGen.generateSchema(type);
+            PrintWriter writer = new PrintWriter(type.getSimpleName() + "_schema.json", "UTF-8");
+            writer.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(schema));
+            writer.close();
+
+        }catch (JsonMappingException ex) {
+
+        }catch (IOException ex) {
+
+        }
     }
 
     private static void testEventProcessing() {
