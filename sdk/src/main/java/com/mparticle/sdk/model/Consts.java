11bf06c440c013764885e29c05f08a3ddf72ab99
mbenua-mparticle <mbenua@mparticle.com>
2018-07-10 13:55:14 -0700

    Increment version to 1.10.0

--- sdk/src/main/java/com/mparticle/sdk/model/Consts.java
+++ sdk/src/main/java/com/mparticle/sdk/model/Consts.java
@@ -2,7 +2,7 @@ package com.mparticle.sdk.model;
 
 public final class Consts {
 
-    public static final String SDK_VERSION= "1.9.1";
+    public static final String SDK_VERSION= "1.10.0";
 
     private Consts() {
     }
