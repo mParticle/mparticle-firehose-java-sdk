73e249a57171c498001be8abb589fb6cad7384fe
Keanan Frazer <kfrazer@mparticle.com>
2020-04-15 12:39:15 -0700

    Update Version to 2.4.0

--- sdk/src/main/java/com/mparticle/sdk/model/Consts.java
+++ sdk/src/main/java/com/mparticle/sdk/model/Consts.java
@@ -2,7 +2,7 @@ package com.mparticle.sdk.model;
 
 public final class Consts {
 
-    public static final String SDK_VERSION= "2.3.0";
+    public static final String SDK_VERSION= "2.4.0";
 
     private Consts() {
     }
