3c46c92299f93e88f33eb59fbeae3444ebeda3fe
Guillermo Tamanaha <v-gtamanaha@mparticle.com>
2022-02-22 10:48:23 -0300

    Update SDK Version to 2.8.0

--- sdk/src/main/java/com/mparticle/sdk/model/Consts.java
+++ sdk/src/main/java/com/mparticle/sdk/model/Consts.java
@@ -2,7 +2,7 @@ package com.mparticle.sdk.model;
 
 public final class Consts {
 
-    public static final String SDK_VERSION= "2.7.0";
+    public static final String SDK_VERSION= "2.8.0";
 
     private Consts() {
     }
