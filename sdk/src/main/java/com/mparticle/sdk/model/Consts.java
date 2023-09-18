3cc2d82758c9e1c5870e5f19fa34954b84d0d34e
Ignacio Linari <v-ilinari@mparticle.com>
2023-09-18 09:25:58 -0300

    Update SDK Version to 3.0.0

--- sdk/src/main/java/com/mparticle/sdk/model/Consts.java
+++ sdk/src/main/java/com/mparticle/sdk/model/Consts.java
@@ -2,7 +2,7 @@ package com.mparticle.sdk.model;
 
 public final class Consts {
 
-    public static final String SDK_VERSION= "2.9.3";
+    public static final String SDK_VERSION= "3.0.0";
 
     private Consts() {
     }
